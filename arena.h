4cee1f87077f7f5595500ccb6988191590eeebca
Eric Milan <emilan@ericmilan.dev>
2026-05-12 22:33:01 -0400

    Arena impl

---
+++ arena.h
@@ -0,0 +1,31 @@
+#include "base/base.h"
+
+#define ARENA_BASE_POS (sizeof(mem_arena))
+#define ARENA_ALIGN (sizeof(void *))
+#define PUSH_STRUCT(arena, T) (T *)arena_push((arena), sizeof(T), false)
+#define PUSH_STRUCT_NZ(arena, T) (T *)arena_push((arena), sizeof(T), true)
+#define PUSH_ARRAY(arena, T, n) (T *)arena_push((arena), sizeof(T) * (n), false)
+#define PUSH_ARRAY_NZ(arena, T, n)                                             \
+  (T *)arena_push((arena), sizeof(T) * (n), true)
+
+typedef struct {
+  u64 reserve_size;
+  u64 commit_size;
+
+  u64 pos;
+  u64 commit_pos;
+} mem_arena;
+
+mem_arena *arena_create(u64 reserve_size, u64 commit_size);
+void arena_destroy(mem_arena *arena);
+void *arena_push(mem_arena *arena, u64 size, b32 non_zero);
+void arena_pop(mem_arena *arena, u64 size);
+void arena_pop_to(mem_arena *arena, u64 pos);
+void arena_clear(mem_arena *arena);
+
+u32 plat_get_pagesize(void);
+
+void *plat_mem_reserve(u64 size);
+b32 plat_mem_commit(void *ptr, u64 size);
+b32 plat_mem_decommit(void *ptr, u64 size);
+b32 plat_mem_release(void *ptr, u64 size);
